162177d6d4d1dc99e93d75bca3e7f09bab75539a
Nathan <nathan@hprl.dev>
2026-01-27 22:39:03 -0500

    fix: wrap header content in a header tag for better semantic structure

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,7 +3,9 @@ import ThemeSwitcher from '@/components/ThemeSwitcher.vue'
 </script>
 
 <template>
-  <img src="/oe.png" alt="DoToo" width="42" height="42" class="fixed top-4 left-6 z-10">
+  <header>
+    <img src="/oe.png" alt="DoToo" width="42" height="42" class="fixed top-4 left-6 z-10">
 
-  <ThemeSwitcher />
+    <ThemeSwitcher />
+  </header>
 </template>
